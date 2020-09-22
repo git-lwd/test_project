207403ade4bfbba763f2f3b4441605243d49f1a6
git-lwd <1689588235@qq.com>
2020-09-22 08:45:49 +0800

    提交

--- routes/upload.js
+++ routes/upload.js
@@ -1,36 +1,55 @@
 const router = require('koa-router')()
 const multer = require('koa-multer');
 const path = require('path')
-class UploadRoutes {
-    static initRoutes() {
-        var storage = multer.diskStorage({
-            //文件保存路径
-            destination: function (req, file, cb) {
-                let filepath = path.join(__dirname, '../public/uploads/');
+const fs = require('fs')
 
-                cb(null, filepath)  //注意路径必须存在
-            },
-            //修改文件名称
-            filename: function (req, file, cb) {
-                var fileFormat = (file.originalname).split(".");
-                cb(null, Date.now() + "." + fileFormat[fileFormat.length - 1]);
-            }
-        })
-        //加载配置
-        var upload = multer({ storage: storage })
-        router.post('/upload', upload.single('file'), async (ctx, next) => {
-            console.log(ctx)
-            try {
-                ctx.body = {
-                    filepath: ctx.req.file.filename,//返回文件名
-                }
-            } catch (error) {
-                ctx.throw(500, error)
-                next()
-            }
-        })
-        return router
+//递归创建文件目录
+var checkDirExist = (filePath, suc) => {
+    var pathArr = filePath.split("\\");
+    let _path = "";
+    for (var i = 0; i < pathArr.length; i++) {
+        _path += (i == 0 ? `${pathArr[i]}` : `/${pathArr[i]}`);
+        if (!fs.existsSync(_path)) {
+            fs.mkdirSync(_path);
+        }
+    }
+    if (suc) {
+        suc();
     }
 }
 
-module.exports = UploadRoutes.initRoutes()
+//上传配置
+var storage = multer.diskStorage({
+    //文件保存路径
+    destination: function (req, file, cb) {
+        var date = new Date();
+        let filePath = "D:/upload/img/" + date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+        filePath = path.relative(__dirname, filePath);
+        checkDirExist(filePath, () => {
+            cb(null, filePath)
+        })
+    },
+    //修改文件名称
+    filename: function (req, file, cb) {
+        var fileFormat = (file.originalname).split(".");
+        cb(null, Date.now() + "." + fileFormat[fileFormat.length - 1]);
+    }
+})
+
+//加载配置
+var upload = multer({ storage: storage })
+
+router.post('/upload', upload.single('file'), async (ctx, next) => {
+    console.log(ctx)
+    try {
+        let url = ctx.req.file.path.split('upload')[1].replace(/\\/g, '/')
+        ctx.body = {
+            path: url
+        }
+    } catch (error) {
+        ctx.throw(500, error)
+        next()
+    }
+})
+
+module.exports = router
